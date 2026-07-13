d488321688ebdc5c71de0bcdb4e6e574401874bf
Impulse Digital <collabs@theimpulsedigital.com>
2026-07-13 18:31:19 +0530

    feat: implement root layout with regional metadata, tracking scripts, and Zoho blocking logic

--- app/layout.tsx
+++ app/layout.tsx
@@ -119,18 +119,6 @@ export default async function RootLayout({
                 `,
               }}
             />
-            {/* Google Tag Manager */}
-            <script
-              dangerouslySetInnerHTML={{
-                __html: `
-                  (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                  })(window,document,'script','dataLayer','GTM-5Z8KMKBC');
-                `,
-              }}
-            />
           </>
         )}
         {/* Favicon */}
@@ -201,12 +189,24 @@ export default async function RootLayout({
                 `,
               }}
             />
+            {/* Google Tag Manager */}
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `
+                  (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                  })(window,document,'script','dataLayer','GTM-5Z8KMKBC');
+                `,
+              }}
+            />
           </>
         )}
       </head>
       <body>
-        {/* Google Tag Manager (noscript) for AE only */}
-        {region === 'ae' && (
+        {/* Google Tag Manager (noscript) for India only */}
+        {region !== 'ae' && (
           <noscript>
             <iframe 
               src="https://www.googletagmanager.com/ns.html?id=GTM-5Z8KMKBC"
