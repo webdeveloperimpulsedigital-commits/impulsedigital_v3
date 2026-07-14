ca535d8e5c2780ba613691e63c687f210457f310
Impulse Digital User <user@impulsedigital.com>
2026-07-14 11:51:14 +0530

    feat: add InteractionLoader to defer heavy script loading until user interaction or timeout

--- components/InteractionLoader.tsx
+++ components/InteractionLoader.tsx
@@ -61,13 +61,6 @@ export default function InteractionLoader() {
     const triggerLoad = () => {
       loadScripts();
       
-      // Load Google Tag Manager
-      if (!document.getElementById('gtm-script')) {
-        const gtmScript = document.createElement('script');
-        gtmScript.id = 'gtm-script';
-        gtmScript.innerHTML = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-M4TW43X3');`;
-        document.head.appendChild(gtmScript);
-      }
 
       // Switch FontAwesome from print to all
       document.querySelectorAll('link[rel="stylesheet"][media="print"]').forEach((l: any) => {
